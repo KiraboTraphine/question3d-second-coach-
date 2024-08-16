1fd264164fa07f1e40c82561cdbca09ab99225a9
KiraboTraphine <kirabodaphinetraphine@gmail.com>
2024-08-16 13:41:29 +0300

    Add files via upload

---
+++ src/secondcoach/Secondcoach.java
@@ -0,0 +1,28 @@
+
+package secondcoach;
+
+
+public class Secondcoach {
+
+        public static void main(String[] args) {
+        int totalDistanceKm = 10000; // in km
+        double speedMetersPerSecond = 225.5; // in m/s
+
+        // Convert distance to meters and speed to km/h
+        double totalDistanceMeters = totalDistanceKm * 1000; // in meters
+        double speedKmPerHour = speedMetersPerSecond * 3.6; // convert m/s to km/h
+
+        double travelTimeHours = totalDistanceMeters / (speedKmPerHour * 1000); // total time in hours
+
+        int startTimeHour = 9; // starting at 09:00 hrs
+
+        double arrivalTime = startTimeHour + travelTimeHours;
+
+        System.out.println("The second coach will arrive at approximately " + arrivalTime + " hours.");
+    }
+}
+
+       
+    
+    
+
